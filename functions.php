0b5c80fb2fc6bbc8ed1703b669a0b49db8a40c52
RogerTM <roger213tm@gmail.com>
2018-10-12 10:28:06 -0400

    Commit inicial para WYVF

--- functions.php
+++ functions.php
@@ -1,45 +1,45 @@
 <?php
 /**
- * Twenty'em Child
+ * WYVF
  *
  * @package			WordPress
- * @subpackage		Twenty'em Child
+ * @subpackage		WYVF
  * @author			RogerTM
  * @license			license.txt
  * @link			https://themingisprose.com/twenty-em
- * @since 			Twenty'em Child 1.0
+ * @since 			WYVF 1.0
  */
 
 /**
- * Twenty'em Child Setup
+ * WYVF Setup
  *
- * @since Twenty'em Child 1.0
+ * @since WYVF 1.0
  */
-function t_em_child_setup(){
-	// Make Twenty'em Child available for translation.
-	load_child_theme_textdomain( 't_em_child', get_stylesheet_directory() . '/languages' );
+function wyvf_setup(){
+	// Make WYVF available for translation.
+	load_child_theme_textdomain( 'wyvf', get_stylesheet_directory() . '/languages' );
 }
-add_action( 'after_setup_theme', 't_em_child_setup' );
+add_action( 'after_setup_theme', 'wyvf_setup' );
 
 /**
  * Enqueue and register all css and js
  *
- * @since Twenty'em Child 1.0
+ * @since WYVF 1.0
  */
-function t_em_child_enqueue(){
-	wp_register_style( 't_em_child-', t_em_get_css( 'theme', T_EM_CHILD_THEME_DIR_PATH .'/css', T_EM_CHILD_THEME_DIR_URL .'/css' ), '', t_em_theme( 'Version' ), 'all' );
-	wp_enqueue_style( 't_em_child-' );
+function wyvf_enqueue(){
+	wp_register_style( 'wyvf-', t_em_get_css( 'theme', T_EM_CHILD_THEME_DIR_PATH .'/css', T_EM_CHILD_THEME_DIR_URL .'/css' ), '', t_em_theme( 'Version' ), 'all' );
+	wp_enqueue_style( 'wyvf-' );
 }
-add_action( 'wp_enqueue_scripts', 't_em_child_enqueue' );
+add_action( 'wp_enqueue_scripts', 'wyvf_enqueue' );
 
 /**
  * Dequeue styles form parent theme
  *
- * @since Twenty'em Child 1.2
+ * @since WYVF 1.2
  */
-function t_em_child_dequeue(){
+function wyvf_dequeue(){
 	wp_dequeue_style( 'twenty-em-style' );
 	wp_deregister_style( 'twenty-em-style' );
 }
-add_action( 'wp_enqueue_scripts', 't_em_child_dequeue', 999 );
+add_action( 'wp_enqueue_scripts', 'wyvf_dequeue', 999 );
 ?>
